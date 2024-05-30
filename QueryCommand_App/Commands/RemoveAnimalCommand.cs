b4cde20f5b819237ffaeb11593ce0529b2eedb72
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-05-30 15:26:22 +0200

    Upd

--- QueryCommand_App/Commands/RemoveAnimalCommand.cs
+++ QueryCommand_App/Commands/RemoveAnimalCommand.cs
@@ -3,11 +3,11 @@ using QueryCommand_App.Queries;
 
 namespace QueryCommand_App.Commands;
 
-public class RemoveAnimal<T>(int id, T query):IGenericCommand<T> where T: INormalQuery
+public class RemoveAnimalCommand<T>(int id, T query):IGenericCommand<T> where T: INormalQuery
 {
-    public void Test(RemoveAnimalCommand tRemoveAnimalCommand)
+    public void Test(TQuery tQuery)
     {
-        tRemoveAnimalCommand.Test();
+        tQuery.Test();
         query.MethodFromQueryInterface();
 
     }
@@ -17,7 +17,7 @@ public interface IGenericCommand<T>
 {
 }
 
-public class RemoveAnimalCommand
+public class TQuery
 {
     public void Test()
     {
